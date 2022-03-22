8a04586188179f40e43e02bf849a50303ca920c1
Diganta Som <itsdsom111@gmail.com>
2022-03-23 00:55:44 +0530

    Setting up express server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello Diganta!');
+});
+
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server running on port http://localhost:${PORT}/`)
+    );
+  })
+  .catch(error => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
