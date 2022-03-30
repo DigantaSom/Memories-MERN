83b755d08623cc0de078b493e23aab0dd8119cc6
Diganta Som <itsdsom111@gmail.com>
2022-03-30 21:32:49 +0530

    Added pagination, added some more routes, and refactored the Home component to include pagination and search functionality UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import PostDetails from './components/PostDetails/PostDetails';
 import Auth from './components/Auth/Auth';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, []);
+
   return (
     <BrowserRouter>
-      <Container maxWidth='lg'>
+      <Container maxWidth='xl'>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/auth' element={<Auth />} />
+          <Route path='/' element={<Navigate to='/posts' replace />} />
+          <Route path='/posts' element={<Home />} />
+          <Route path='/posts/search' element={<Home />} />
+          <Route path='/posts/:id' element={<PostDetails />} />
+          <Route
+            path='/auth'
+            element={!user ? <Auth /> : <Navigate to='/posts' replace />}
+          />
         </Routes>
       </Container>
     </BrowserRouter>
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,38 +1,114 @@
 import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { Grow, Container, Grid } from '@material-ui/core';
+import {
+  Grow,
+  Container,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from '@material-ui/core';
+import ChipInput from 'material-ui-chip-input';
 
 import { getPosts } from '../../actions/posts';
 
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Pagination from '../Pagination/Pagination';
 
 import useStyles from './styles';
 
+const useQuery = () => new URLSearchParams(useLocation().search);
+
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [tags, setTags] = useState([]);
   const dispatch = useDispatch();
+  const query = useQuery();
+  const navigate = useNavigate();
   const classes = useStyles();
+
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery');
 
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch, currentId]);
 
+  const searchPosts = () => {
+    if (searchTerm.trim()) {
+      // TODO: dispatch -> fetch search post(s)
+    } else {
+      navigate('/');
+    }
+  };
+
+  const handleKeyUp = e => {
+    if (e.key === 'Enter') {
+      searchPosts();
+    }
+  };
+
+  const handleAddTag = tag => {
+    setTags(prevTagsState => [...prevTagsState, tag]);
+  };
+
+  const handleDeleteTag = tagToDelete => {
+    setTags(prevTagsState => prevTagsState.filter(tag => tag !== tagToDelete));
+  };
+
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth='xl'>
         <Grid
           container
           justifyContent='space-between'
           alignItems='stretch'
           spacing={3}
-          className={classes.mainContainer}
+          className={classes.gridContainer}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} md={9} sm={6}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} md={3} sm={6}>
+            <AppBar
+              className={classes.appBarSearch}
+              position='static'
+              color='inherit'
+            >
+              <TextField
+                name='search'
+                variant='outlined'
+                label='Search Memories'
+                fullWidth
+                value={searchTerm}
+                onChange={e => setSearchTerm(e.target.value)}
+                onKeyUp={handleKeyUp}
+              />
+              <ChipInput
+                style={{ margin: '10px 0' }}
+                value={tags}
+                onAdd={handleAddTag}
+                onDelete={handleDeleteTag}
+                label='Search Tags'
+                variant='outlined'
+              />
+              <Button
+                onClick={searchPosts}
+                className={classes.searchButton}
+                variant='contained'
+                color='primary'
+              >
+                Search
+              </Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper className={classes.pagination} elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
